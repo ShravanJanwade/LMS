2920629be7569bb061670457b69403d9431aa67f
Shravan <Shravan@gmail.com>
2024-04-16 09:13:05 +0530

    Backup

--- src/Services/BatchDetailsData.js
+++ src/Services/BatchDetailsData.js
@@ -14,26 +14,27 @@ export async function fetchBatchDetails(batchId) {
 }
 // BatchEmployee.js
 export async function deleteTraineesFromBatch(batchId, selectedUsers) {
-  console.log({userIds:selectedUsers});
+  console.log(selectedUsers);
   try {
     const response = await fetch(`http://localhost:1212/batches/${batchId}/users`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({userIds:selectedUsers}), // send the selectedUsers directly
+      body: JSON.stringify(selectedUsers), // send the selectedUsers directly
     });
     if (!response.ok) {
-      console.log("Couldn't delete")
+      console.log("Couldn't delete");
       throw new Error("Failed to delete trainees from batch");
     }
     // Return response data if needed
-    console.log(response.json());
+    console.log(await response.json());
     return response.json();
   } catch (error) {
     throw new Error(`Error deleting trainees from batch: ${error.message}`);
   }
 }
+
 // deleteBatch.js
 export async function deleteBatch(batchId) {
   try {

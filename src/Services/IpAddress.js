1bd50b482cd0c05ff5108931a6c0c5f0b30192ac
Shravan <Shravan@gmail.com>
2024-06-05 10:25:35 +0530

    latest

--- src/Services/IpAddress.js
+++ src/Services/IpAddress.js
@@ -3,10 +3,10 @@ export const ProgressIp="http://172.18.5.20:8085"
 // export const BatchIp="http://172.18.4.243:8090";
 // export const ProgressIp="http://172.18.4.233:2222";
 // export const ProgressIp="http://172.18.4.233:2222";
-// export const AttendanceIp = "http://172.18.5.20:8085/attendance";
-export const AttendanceIp = "http://172.18.4.243:1215/attendance";
+export const AttendanceIp = "http://172.18.5.20:8085/attendance";
+// export const AttendanceIp = "http://172.18.4.243:1215/attendance";
 export const LearningPlanIp = "http://172.18.5.20:8085";
-export const token="eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJQcmFuYXkiLCJyb2xlIjoiQURNSU4iLCJlbXBsb3llZUlkIjo3NDk3LCJzdWIiOiJwcmFuYXlfcmVkZHlAdGhicy5jb20iLCJpYXQiOjE3MTcxMjcxNjMsImV4cCI6MTcyMDcyNzE2M30.uiTYjzX739GxqLyMONe-sePx25cH0hujm26YIbLgiPg"
+export const token="eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJCaGFyYXRlc2giLCJyb2xlIjoiVVNFUiIsImVtcGxveWVlSWQiOjc0ODEsInN1YiI6ImJoYXJhdGVzaF9raGFydmlAdGhicy5jb20iLCJpYXQiOjE3MTc0ODIwNTgsImV4cCI6MTcyMTA4MjA1OH0.BqHy0OXAooLdBAbq0vlNmaNj70Dpmzr9_xyQfFeH_vQ"
 export const myHeaders = new Headers({
     "Content-Type": "application/json",
     "Authorization": `Bearer ${token}`,
